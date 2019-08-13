92e0a900a6bf0fd32ade341d1179404fce8a324d
Ryan Crawford <RyanCCrawford@live.com>
2019-08-13 14:06:42 -0400

    Restructured Folders

--- server.js
+++ server.js
@@ -8,17 +8,27 @@ const passport = require("passport");
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//CORS middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+// Passport middleware
+app.use(passport.initialize());
+
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Passport middleware
-app.use(passport.initialize());
+
+
 // Passport config
 require("./config/passport")(passport);
 
-require("./routes/api/playersRoutes")(app);
+// Routes
 require("./routes/api/apiRoutes")(app);
 
 
